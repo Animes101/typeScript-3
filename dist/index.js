f2c78b18da5e0b752d079edfcc6b5d0839c51ded
Animes <animesbarman101@gmail.com>
2025-12-28 11:31:18 +0600

    add some

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,27 @@
 "use strict";
 // //object orianterd programming
 Object.defineProperty(exports, "__esModule", { value: true });
-// //class
-// class Student{
-//     // name:string;
-//     // age:number;
-//     // email:string;
-//     //jparamete properties
-//     constructor(public name:string,public age:number,public email:string){
-//         // this.name=name;
-//         // this.age=age;
-//         // this.email=email
-//     }
-//     showStudent(){
-//         console.log(`Student name: ${this.name}, age:${this.age},  email:${this.email}`)
-//     }
-// }
-// const student1=new Student('animes', 23, 'animes@gamil.com');
-// const student2=new Student('rony ', 20, 'animes101@gamil.com');
-// student1.showStudent();
-// student2.showStudent()
-//opp --inheritence
-class Student {
-    name;
-    age;
-    email;
-    address;
-    constructor(name, age, email, address) {
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.address = address;
+const add = (x, y) => {
+    if (typeof x === 'number' && typeof y === 'number') {
+        console.log(x + y);
     }
-    showData() {
-        console.log(`name:${this.name}, email:${this.email}, address:${this.address}, age:${this.age}`);
+    else if (typeof x === 'string' && typeof y === 'string') {
+        console.log(`${x} ${y}`);
     }
-}
-const Student1 = new Student('animes barman', 20, 'animnes@gamil.com', 'thakurgaon');
-const Student2 = new Student('jobon barman', 30, 'animnes102@gamil.com', 'ruhia');
-class Teacher {
-    name;
-    age;
-    email;
-    address;
-    designation;
-    constructor(name, age, email, address, designation) {
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.address = address;
-        this.designation = designation;
+};
+add('xxx', 'yy');
+add(20, 30);
+const getUser = (user) => {
+    if ('role' in user) {
+        console.log('user Admin');
     }
-    showData(numberOfshllep) {
-        console.log(`name:${this.name}, email:${this.email}, address:${this.address}, age:${this.age}, sleep:${numberOfshllep}`);
-    }
-}
-const teacher1 = new Teacher('animes barman', 20, 'animnes@gamil.com', 'thakurgaon', 'teacher');
-const Teacher2 = new Teacher('jobon barman', 30, 'animnes102@gamil.com', 'ruhia', 'teacher');
-teacher1.showData(20);
+    console.log('user');
+};
+const normalUser = {
+    name: 'animes'
+};
+const adminUser = {
+    name: 'admin user',
+    role: 'admin'
+};
+getUser(adminUser);
--- src/index.ts
+++ src/index.ts
@@ -34,59 +34,119 @@
 //opp --inheritence
 
 
-class Parent{
+// class Parent{
 
-       name:string;
-    age:number;
-    email:string;
-    address:string;
+//        name:string;
+//     age:number;
+//     email:string;
+//     address:string;
 
 
-    constructor(name:string, age:number, email:string, address:string){
-        this.name=name;
-        this.age=age;
-        this.email=email;
-        this.address=address;
-    }
+//     constructor(name:string, age:number, email:string, address:string){
+//         this.name=name;
+//         this.age=age;
+//         this.email=email;
+//         this.address=address;
+//     }
 
-    showData(sleepTime:number){
-        console.log(`name:${this.name}, email:${this.email}, address:${this.address}, age:${this.age}, sleepTime:${sleepTime}`)
-    }
+//     showData(sleepTime:number){
+//         console.log(`name:${this.name}, email:${this.email}, address:${this.address}, age:${this.age}, sleepTime:${sleepTime}`)
+//     }
 
-}
+// }
 
-class Student extends Parent{
+// class Student extends Parent{
 
-    constructor(name:string, age:number, email:string, address:string){
-        super(name,age ,email,address)
-    }
+//     constructor(name:string, age:number, email:string, address:string){
+//         super(name,age ,email,address)
+//     }
 
    
  
-}
+// }
 
 
-const Student1=new Student('animes barman', 20, 'animnes@gamil.com', 'thakurgaon')
-const Student2=new Student('jobon barman', 30, 'animnes102@gamil.com', 'ruhia')
+// const Student1=new Student('animes barman', 20, 'animnes@gamil.com', 'thakurgaon')
+// const Student2=new Student('jobon barman', 30, 'animnes102@gamil.com', 'ruhia')
 
-class Teacher extends Parent{
+// class Teacher extends Parent{
    
-    designation:string;
+//     designation:string;
 
 
-    constructor(name:string, age:number, email:string, address:string, designation:string){
-        super(name, age, email, address,);
-        this.designation=designation;
-    }
-   teacher(){
+//     constructor(name:string, age:number, email:string, address:string, designation:string){
+//         super(name, age, email, address,);
+//         this.designation=designation;
+//     }
+//    teacher(){
 
-    console.log(`${this.designation}`)
+//     console.log(`${this.designation}`)
 
-   }
+//    }
 
   
+// }
+
+
+// const teacher1=new Teacher('animes barman', 20, 'animnes@gamil.com', 'thakurgaon', 'teacher bangla')
+// const Teacher2=new Teacher('jobon barman', 30, 'animnes102@gamil.com', 'ruhia', 'teacher english')
+
+
+
+// teacher1.teacher()
+
+// type gurds
+
+type Parems=string | number
+
+const add=(x:Parems , y:Parems)=>{
+
+    if(typeof x=== 'number' && typeof y==='number'){
+
+        console.log(x+y)
+    }else if(typeof x === 'string' && typeof y === 'string'){
+        console.log(`${x} ${y}`)
+    }
+
 }
 
 
-const teacher1=new Teacher('animes barman', 20, 'animnes@gamil.com', 'thakurgaon', 'teacher bangla')
-const Teacher2=new Teacher('jobon barman', 30, 'animnes102@gamil.com', 'ruhia', 'teacher english')
+add('xxx', 'yy');
+add(20,30)
+
+
+//in garud 
+
+type NormalUser={
+    name:string;
+}
+
+type AdminUser={
+    name:string;
+    role:'admin'
+}
+
+const getUser=(user:NormalUser | AdminUser)=>{
+
+
+    if('role' in user){
+        console.log('user Admin')
+    }
+
+    console.log('user')
+
+
+}
+
+const normalUser:NormalUser={
+    name:'animes'
+}
+
+const adminUser:AdminUser={
+    name:'admin user',
+    role:'admin'
+}
+
+getUser(adminUser)
+
+
